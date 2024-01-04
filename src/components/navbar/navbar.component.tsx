bb63380828448215e765508b35517478b1c6a432
8suraj <117096420+tkcollabifirst@users.noreply.github.com>
2024-01-04 20:38:38 +0530

    -

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -1,17 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import logo from '../../assets/svgs/logo.svg';
+import humburger from '../../assets/svgs/humburger.svg';
 
 export default function Navbar() {
 	return (
 		<>
 			<nav className='flex justify-between items-center rounded-xl m-5 p-4 bg-black2 backdrop-blur-xl		 md:hidden '>
 				<div>
-					<img src='/public/logo.svg' alt='logo' />
+					<img src={logo} alt='logo' />
 				</div>
 				<div>
-					<img
-						src='/src/assets/svgs/humburger.svg'
-						alt='logo'
-					/>
+					<img src={humburger} alt='logo' />
 				</div>
 			</nav>
 			<Outlet />
