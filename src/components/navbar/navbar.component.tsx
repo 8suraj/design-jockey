65d9e2f6092a40070cd9b17e7d0af7cb4cd1790b
8suraj <117096420+tkcollabifirst@users.noreply.github.com>
2024-02-07 22:23:23 +0530

    -

--- src/components/navbar/navbar.component.tsx
+++ src/components/navbar/navbar.component.tsx
@@ -4,13 +4,17 @@ import humburger from '../../assets/svgs/humburger.svg';
 import Button from '../button/button.component';
 import Hero1 from '../hero/hero1.component';
 import './hero1.css';
-import { DotLoader } from 'react-spinners';
+// import { DotLoader } from 'react-spinners';
 import { useState } from 'react';
+import { Player } from '@lottiefiles/react-lottie-player';
+import animation from '../../Frame_2_1.json';
 
 export default function Navbar() {
 	const [isLoaded, setIsLoaded] = useState(false);
 	const handleImageLoaded = () => {
-		setTimeout(() => setIsLoaded(true), 1000);
+		setTimeout(() => {
+			setIsLoaded(true);
+		}, 1000);
 	};
 
 	return (
@@ -24,46 +28,51 @@ export default function Navbar() {
 					height='100%'
 					onLoad={handleImageLoaded}
 					className={`${
-						isLoaded ? 'OP1' : ''
+						isLoaded ? 'OP1' : 'hidden'
 					} absolute left-0 top-0 `}
 				/>
-
 				<div
 					className={`${
-						isLoaded ? 'OP2 hidden' : ''
-					} w-full h-full flex items-center justify-center absolute left-0 top-0 `}>
-					<div className={`w-fit h-[40vh]  mx-auto`}>
-						<DotLoader color='#36d7b7' />
-					</div>
+						isLoaded ? 'OP2 hidden ' : ''
+					} w-full h-full flex items-center justify-center absolute left-0 top-0  `}>
+					<Player
+						src={animation}
+						className='player'
+						loop
+						autoplay
+					/>
 				</div>
-
-				<nav className='flex items-center rounded-xl     bg-black2 backdrop-blur-xl	p-4  md:p-0	'>
-					<div className='flex w-full  justify-between items-center'>
-						<Link className='h-full  md:pl-2' to='/'>
-							<img
-								src={logo}
-								alt='logo'
-								className='w-8 md:w-10 '
+				{isLoaded ? (
+					<nav className='flex items-center rounded-xl     bg-black2 backdrop-blur-xl	p-4  md:p-0	'>
+						<div className='flex w-full  justify-between items-center'>
+							<Link className='h-full  md:pl-2' to='/'>
+								<img
+									src={logo}
+									alt='logo'
+									className='w-8 md:w-10 '
+								/>
+							</Link>
+							<ul className=' text-color-primary items-center gap-4 m1:text-[.65rem] text-[.65rem]   md:text-[.75rem] hidden md:flex'>
+								<Link to='/cart'>store </Link>
+								<Link to=''> work </Link>
+								<Link to=''>serices</Link>
+								<Link to=''>plans </Link>
+								<Link to='/cart'>cart</Link>
+							</ul>
+							<div className='md:hidden '>
+								<img src={humburger} alt='logo' />
+							</div>
+							<Button
+								text='login'
+								className='btn_base  bg-black4  hidden md:block '
+								onClick={(e) => console.log(e)}
 							/>
-						</Link>
-						<ul className=' text-color-primary items-center gap-4 m1:text-[.65rem] text-[.65rem]   md:text-[.75rem] hidden md:flex'>
-							<Link to='/cart'>store </Link>
-							<Link to=''> work </Link>
-							<Link to=''>serices</Link>
-							<Link to=''>plans </Link>
-							<Link to='/cart'>cart</Link>
-						</ul>
-						<div className='md:hidden '>
-							<img src={humburger} alt='logo' />
 						</div>
-						<Button
-							text='login'
-							className='btn_base  bg-black4  hidden md:block '
-							onClick={(e) => console.log(e)}
-						/>
-					</div>
-				</nav>
-				<Hero1 />
+					</nav>
+				) : (
+					''
+				)}
+				{isLoaded ? <Hero1 /> : ''}
 			</div>
 
 			<Outlet />
